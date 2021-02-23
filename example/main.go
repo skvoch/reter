f5646e3ec5774cd6107c39706fc5877b182ea975
prokudin.dmitriy <prokudin.dmitriy@wildberries.ru>
2021-02-23 16:03:31 +0300

    fix: example

--- example/main.go
+++ example/main.go
@@ -27,11 +27,10 @@ func NotifySigterm() error {
 func main() {
 	s, err := scheduler.New(scheduler.Zerolog(log.Logger), &scheduler.Options{
 		Etcd: scheduler.EtcdOptions{
-			Endpoints:   []string{"127.0.0.1:2379"},
-			DialTimeout: time.Second * 5,
-			Timeout:     time.Second * 5,
+			Endpoints: []string{"127.0.0.1:2379"},
 		},
 		LockTTL: time.Minute * 1,
+		Timeout: time.Second * 10,
 	})
 
 	if err != nil {
